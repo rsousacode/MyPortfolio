6ebd5dfe11a43429eca93584d737cd26e0634ce4
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-11-03 20:44:54 +0000

    Refactor and style improvements

--- src/simple-portfolio/src/App.svelte
+++ src/simple-portfolio/src/App.svelte
@@ -1,142 +1,153 @@
 <script>
-  import { fly, fade } from 'svelte/transition'
-  import TypeWriter from 'svelte-typewriter'
-  // props data
-  export let author
-  export let position
-  export let projects
-  export let twitter
-  export let linkedin
-  export let github
+    import {fly, fade} from 'svelte/transition'
+    import TypeWriter from 'svelte-typewriter'
+    // props data
+    export let author
+    export let position
+    export let projects
+    export let twitter
+    export let linkedin
+    export let github
 
-  // auxiliar 
-  let termPrompt = '> '
-  let termText = termPrompt + 'Loading...'
-  let pageLoaded = false
-  let timeouts = []
+    // auxiliar
+    let termPrompt = '> '
+    let termText = termPrompt + 'Loading...'
+    let pageLoaded = false
+    let timeouts = []
 
-  // functions
-  function getProjectVisibility (name) {
-    return projects.filter(d => d.name === name)[0].visible
-  }
-
-  function toggleProjectVisibility (name) {
-
-    const index = projects.findIndex(p => p.name === name)
-    projects[index].visible = !projects[index].visible
-    if(projects[index].visible) {
-      clearTimeouts()
-      typeWriteAfter(2500, `Stack: ${projects[index].tech}`)
-    }  else {
-      clearTimeouts()
-      typeWriteAfter(30, defaultTermText)
+    // functions
+    function getProjectVisibility(name) {
+        return projects.filter(d => d.name === name)[0].visible
     }
 
-  }
+    function toggleProjectVisibility(name) {
 
+        const index = projects.findIndex(p => p.name === name)
+        projects[index].visible = !projects[index].visible
+        if (projects[index].visible) {
+            clearTimeouts()
+            typeWriteAfter(2500, `Stack: ${projects[index].tech}`)
+        } else {
+            clearTimeouts()
+            typeWriteAfter(30, defaultTermText)
+        }
 
-  function closeAllVisibilityExcept (name) {
-    toggleProjectVisibility(name)  
-
-    var i;
-    for (i = 0; i < projects.length; i++) {
-      var project = projects[i]
-      if(project.name !== name) {
-        projects[i].visible = false
-      }
-    } 
-  }
-
-  function clearTimeouts () {
-    var i
-    for (i = 0; i < timeouts.length; i++) {
-      clearTimeout(timeouts[i])
     }
-  }
 
-  let defaultTermText = '...'
 
-  function simulateLoading () {
-    setTimeout(() => {
-      pageLoaded = true
-      
-      typeWriteAfter(1000, 'Welcome to my portfolio! :) ')
-    }, 1500)
-  }
+    function closeAllVisibilityExcept(name) {
+        toggleProjectVisibility(name)
 
-  function typeWriteAfter(delay, text) {
-    timeouts.push(setTimeout (() => {
-      typeWriting = true
-      termText = termPrompt + text
-    }, delay))
-  }
+        let i;
+        for (i = 0; i < projects.length; i++) {
+            const project = projects[i]
+            if (project.name !== name) {
+                projects[i].visible = false
+            }
+        }
+    }
 
-  // term
-  let typeWriting = true
+    function clearTimeouts() {
+        let i
+        for (i = 0; i < timeouts.length; i++) {
+            clearTimeout(timeouts[i])
+        }
+    }
 
-  function OnTypeWritingFinished () {
-    setTimeout(() => {
-      typeWriting = false
-    }, 1000)
+    let defaultTermText = '...'
 
-  }
+    function simulateLoading() {
+        setTimeout(() => {
+            pageLoaded = true
 
-  simulateLoading()
-  
+            typeWriteAfter(1000, 'Welcome to my portfolio! :) ')
+        }, 1500)
+    }
+
+    function typeWriteAfter(delay, text) {
+        timeouts.push(setTimeout(() => {
+            typeWriting = true
+            termText = termPrompt + text
+        }, delay))
+    }
+
+    // term
+    let typeWriting = true
+
+    function OnTypeWritingFinished() {
+        setTimeout(() => {
+            typeWriting = false
+        }, 1000)
+
+    }
+
+    simulateLoading()
+
 </script>
 <main>
-      <div class="flex-container-header">
+    <div class="flex-container-header">
         <div class="flex-item">
-    <div class="author-card">
-      <div class="author" transition:fade>{author}</div>
-      
-      <div class="position">{position}</div>
-      <a href="{github}" target="_blank" class="link ml-5">
-        <img alt="github" src="/icons/github-alt.svg" class="icon"/>
-      </a>
-      <a href="{linkedin}" target="_blank" class="link ml-5">
-        <img alt="linkedin" src="/icons/linkedin.svg" class="icon"/>
-      </a>
-      <a href="{twitter}" target="_blank" class="link ml-5"><img alt="twitter" src="/icons/twitter.svg" class="icon"/></a>
+            <div class="author-card">
+                <div class="author" transition:fade>{author}</div>
 
+                <div class="position">{position}</div>
+                <a href="{github}" target="_blank" class="link ml-5">
+                    <img alt="github" src="/icons/github-alt.svg" class="icon"/>
+                </a>
+                <a href="{linkedin}" target="_blank" class="link ml-5">
+                    <img alt="linkedin" src="/icons/linkedin.svg" class="icon"/>
+                </a>
+                <a href="{twitter}" target="_blank" class="link ml-5"><img alt="twitter" src="/icons/twitter.svg"
+                                                                           class="icon"/></a>
+
+            </div>
+        </div> <!-- close flex item-->
     </div>
-    </div> <!-- close flex item-->
-    </div>
-      {#if pageLoaded}
-      <div class="projects-title"  transition:fade>Projects</div>
-        <div class="flex-container-header"  transition:fade>
-          <div class="flex-item">
-            {#each projects as {name, description, id, github}}
-            <div class="stuff" data-flip-key={id} data-flip-no-scale>
-              <h2 class="pointer" data-flip-no-scale on:click={closeAllVisibilityExcept(name)}>
-                {name}</h2>
-                <div class="details">
-                  {#if getProjectVisibility(name)}
-                  <div  transition:fly="{{ x: -100, duration: 700, delay: 500 }}">
-                    <p>{description}</p>
-                    <a transition:fly="{{ y: 200, duration: 700, delay: 1500 }}" href="{github}" target="_blank" class="link"><img alt="github" src="/icons/github-alt.svg" class="icon"/>
-                      <span class="link">GitHub</span>
-                    </a>
-                  </div>
-                  {/if}
-                </div> <!-- close class details -->
-              </div> <!-- close class stuff -->
-            {/each}
-        </div> <!-- close class flex item -->
-      </div> <!-- close class flex container -->
-      {/if}
+    {#if pageLoaded}
+        <div class="flex-container-header" transition:fade>
+            <div class="flex-item">
+              <div class="projects-title" transition:fade>Projects</div>
+
+              {#each projects as {name, description, id, github, website}}
+                    <div class="stuff" data-flip-key={id} data-flip-no-scale>
+                        <h2 class="pointer" data-flip-no-scale on:click={closeAllVisibilityExcept(name)}>
+                            {name}</h2>
+                        <div class="details">
+                            {#if getProjectVisibility(name)}
+                                <div transition:fly="{{ x: -100, duration: 700, delay: 500 }}">
+                                    <p>{description}</p>
+                                    <a transition:fly="{{ y: 200, duration: 700, delay: 1500 }}" href="{github}"
+                                       target="_blank" class="link"><img alt="github" src="/icons/github-alt.svg"
+                                                                         class="icon"/>
+                                        <span class="link">GitHub</span>
+                                    </a>
+                                  {#if website !== ''}
+                                    <a transition:fly="{{ y: 200, duration: 700, delay: 2500 }}" href="{website}"
+                                       target="_blank" class="link"><img alt="website" src="/icons/web.svg"
+                                                                         class="icon"/>
+                                    <span class="link">Website</span>
+                                  </a>
+                                  {/if}
+                                </div>
+                            {/if}
+                        </div> <!-- close class details -->
+                    </div> <!-- close class stuff -->
+                {/each}
+            </div> <!-- close class flex item -->
+        </div> <!-- close class flex container -->
+    {/if}
     <footer>
-      {#if typeWriting}
-      <TypeWriter cursor=true on:done={() => OnTypeWritingFinished()}>
-      <div class="term"> 
-        <div class="term-text">{termText} </div>
-      </div>
-      </TypeWriter>
-      {:else}
-      <div class="term"> 
-        <div class="term-text">{termText} </div>
-      </div>
-      {/if}
+        {#if typeWriting}
+            <TypeWriter cursor=true on:done={() => OnTypeWritingFinished()}>
+                <div class="term">
+                    <div class="term-text">{termText} </div>
+                </div>
+            </TypeWriter>
+        {:else}
+            <div class="term">
+                <div class="term-text">{termText} </div>
+            </div>
+        {/if}
     </footer>
 
 </main>
--- src/simple-portfolio/src/main.js
+++ src/simple-portfolio/src/main.js
@@ -1,84 +1,87 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-    author: 'Rui Sousa',
-    position: "Full-stack programmer",
-    twitter: 'https://twitter.com/rsousacode',
-    github: 'https://github.com/rsousacode',
-    linkedin: 'https://linkedin.com/in/rsousa',
-    uid: 0,
-    projects: [
-      { 
-        id: 0,
-        name: 'Commands and Snippets', 
-        description: 'Web app that allows storing useful snippets and commands. It has a branch with authentication provided. ' ,
-        github:'https://github.com/rsousacode/CommandsAndSnippets', 
-        toggled: false,
-        tech: '.Net Core Mvc 3.x, T-SQL, Angular, Vue.Js, Quasar'
-      },
-      { 
-        id: 1,
-        name: 'Winter Come', 
-        description: 'Backend, for the Winter Come game. The player can do Qr login from the companion app to be able to sign in in the game. ' , 
-        github:'https://github.com/rsousacode/WinterComeBackend',
-        toggled: false,
-        tech: 'Node.JS, Express, Mongoose, MongoDB, C# (Client)'
-      },
-      { 
-        id: 2,
-        name: 'Puryon', 
-        description: 'Frontend and Backend, for the Puryon FPS multiplayer game made in UE4, which presents real-time statistics and data about the game being played.' , 
-        github:'https://github.com/rsousacode/puryon_backend',
-        toggled: false,
-        tech: 'Node.JS, Express, Mongoose, MongoDB, React.Js, C++ (Client)'
+    target: document.body,
+    props: {
+        author: 'Rui Sousa',
+        position: "Software developer",
+        twitter: 'https://twitter.com/rsousacode',
+        github: 'https://github.com/rsousacode',
+        linkedin: 'https://linkedin.com/in/rsousa',
+        uid: 0,
+        projects: [
+            {
+                id: 0,
+                name: 'Commands and Snippets',
+                description: 'Web app that allows storing useful snippets and commands. It has a branch with authentication provided. ',
+                github: 'https://github.com/rsousacode/CommandsAndSnippets',
+                website: '',
+                toggled: false,
+                tech: '.Net Core Mvc 3.x, T-SQL, Angular, Vue.Js, Quasar'
+            },
+            {
+                id: 1,
+                name: 'Winter Come',
+                description: 'Backend, for the Winter Come game. The player can do Qr login from the companion app to be able to sign in in the game. ',
+                github: 'https://github.com/rsousacode/WinterComeBackend',
+                toggled: false,
+                website: 'https://bigmonte.com/projects/wintercome/',
+                tech: 'Node.JS, Express, Mongoose, MongoDB, C# (Client)'
+            },
+            {
+                id: 2,
+                name: 'Puryon',
+                description: 'Frontend and Backend, for the Puryon FPS multiplayer game made in UE4, which presents real-time statistics and data about the game being played.',
+                github: 'https://github.com/rsousacode/puryon_backend',
+                toggled: false,
+                website: 'https://bigmonte.com/projects/puryon/',
+                tech: 'Node.JS, Express, Mongoose, MongoDB, React.Js, C++ (Client)'
 
-      },
-      {
-        id: 3,
-        name: 'Lusoportunas', 
-      description: 'Streamlines the job finding process '
-      + 'and has a comprehensive dashboard for recruiters and job seekers with a communication' 
-      + 'platform that allows both to connect easily.', 
-      github:'https://github.com/rsousacode/Lusoportunas',
-      toggled: false,
-      tech: 'Node.JS, Express, Mongoose, MongoDB, Pug, Passport'
+            },
+            {
+                id: 3,
+                name: 'Lusoportunas',
+                description: 'Streamlines the job finding process '
+                    + 'and has a comprehensive dashboard for recruiters and job seekers with a communication'
+                    + 'platform that allows both to connect easily.',
+                github: 'https://github.com/rsousacode/Lusoportunas',
+                toggled: false,
+                website: '',
+                tech: 'Node.JS, Express, Mongoose, MongoDB, Pug, Passport'
 
-    },
-      
-    ]
-	}
+            },
+
+        ]
+    }
 });
 
 
 const flipping = new Flipping({
-  easing: "cubic-bezier(.01, 0, .5, 1)"
+    easing: "cubic-bezier(.01, 0, .5, 1)"
 });
 
 setTimeout(() => {
-  let currentOpenEl;
-  Array.from(document.querySelectorAll(".pointer"), button => {
-  let parent = button.parentNode
-  console.log(parent)
-  button.addEventListener(
-    "click",
-    flipping.wrap(() => {
+    let currentOpenEl;
+    Array.from(document.querySelectorAll(".pointer"), button => {
+        let parent = button.parentNode
+        console.log(parent)
+        button.addEventListener(
+            "click",
+            flipping.wrap(() => {
 
-      if (parent.dataset.open) {
-        delete parent.dataset.open;
-        currentOpenEl = null;
-      } else {
-        parent.dataset.open = true;
-        if (currentOpenEl) {
-          delete currentOpenEl.dataset.open;
-        }
-        currentOpenEl = parent;
-      }
-    }));
-});
+                if (parent.dataset.open) {
+                    delete parent.dataset.open;
+                    currentOpenEl = null;
+                } else {
+                    parent.dataset.open = true;
+                    if (currentOpenEl) {
+                        delete currentOpenEl.dataset.open;
+                    }
+                    currentOpenEl = parent;
+                }
+            }));
+    });
 }, 1550)
-
 
 
 export default app;
