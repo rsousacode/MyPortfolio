bc326e5d5b6fb4832329928f6c2401e08ede5de9
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-11-03 20:56:08 +0000

    code improvements

--- src/simple-portfolio/src/App.svelte
+++ src/simple-portfolio/src/App.svelte
@@ -85,72 +85,66 @@
 
 </script>
 <main>
-    <div class="flex-container-header">
-        <div class="flex-item">
-            <div class="author-card">
-                <div class="author" transition:fade>{author}</div>
+  <div class="flex-container">
+    <div class="flex-item">
+      <div class="author-card">
+        <div class="author" transition:fade>{author}</div>
 
-                <div class="position">{position}</div>
-                <a href="{github}" target="_blank" class="link ml-5">
-                    <img alt="github" src="/icons/github-alt.svg" class="icon"/>
-                </a>
-                <a href="{linkedin}" target="_blank" class="link ml-5">
-                    <img alt="linkedin" src="/icons/linkedin.svg" class="icon"/>
-                </a>
-                <a href="{twitter}" target="_blank" class="link ml-5"><img alt="twitter" src="/icons/twitter.svg"
-                                                                           class="icon"/></a>
+        <div class="position">{position}</div>
+        <a href="{github}" target="_blank" class="link ml-5">
+          <img alt="github" src="/icons/github-alt.svg" class="icon"/>
+        </a>
+        <a href="{linkedin}" target="_blank" class="link ml-5">
+          <img alt="linkedin" src="/icons/linkedin.svg" class="icon"/>
+        </a>
+        <a href="{twitter}" target="_blank" class="link ml-5"><img alt="twitter" src="/icons/twitter.svg" class="icon"/></a>
+      </div>
+    </div> <!-- close flex item-->
+  </div>
+  {#if pageLoaded}
+    <div class="flex-container" transition:fade>
+      <div class="flex-item">
+        <div class="projects-title" transition:fade>Projects</div>
 
-            </div>
-        </div> <!-- close flex item-->
-    </div>
-    {#if pageLoaded}
-        <div class="flex-container-header" transition:fade>
-            <div class="flex-item">
-              <div class="projects-title" transition:fade>Projects</div>
-
-              {#each projects as {name, description, id, github, website}}
-                    <div class="stuff" data-flip-key={id} data-flip-no-scale>
-                        <h2 class="pointer" data-flip-no-scale on:click={closeAllVisibilityExcept(name)}>
-                            {name}</h2>
-                        <div class="details">
-                            {#if getProjectVisibility(name)}
-                                <div transition:fly="{{ x: -100, duration: 700, delay: 500 }}">
-                                    <p>{description}</p>
-                                    <a transition:fly="{{ y: 200, duration: 700, delay: 1500 }}" href="{github}"
-                                       target="_blank" class="link"><img alt="github" src="/icons/github-alt.svg"
-                                                                         class="icon"/>
-                                        <span class="link">GitHub</span>
-                                    </a>
-                                  {#if website !== ''}
-                                    <a transition:fly="{{ y: 200, duration: 700, delay: 2500 }}" href="{website}"
-                                       target="_blank" class="link"><img alt="website" src="/icons/web.svg"
-                                                                         class="icon"/>
-                                    <span class="link">Website</span>
-                                  </a>
-                                  {/if}
-                                </div>
-                            {/if}
-                        </div> <!-- close class details -->
-                    </div> <!-- close class stuff -->
-                {/each}
-            </div> <!-- close class flex item -->
-        </div> <!-- close class flex container -->
-    {/if}
-    <footer>
-        {#if typeWriting}
-            <TypeWriter cursor=true on:done={() => OnTypeWritingFinished()}>
-                <div class="term">
-                    <div class="term-text">{termText} </div>
+        {#each projects as {name, description, id, github, website}}
+          <div class="stuff" data-flip-key={id} data-flip-no-scale>
+            <h2 class="pointer" data-flip-no-scale on:click={closeAllVisibilityExcept(name)}>
+              {name}</h2>
+            <div class="details">
+              {#if getProjectVisibility(name)}
+                <div transition:fly="{{ x: -100, duration: 700, delay: 500 }}">
+                  <p>{description}</p>
+                  <a transition:fly="{{ y: 200, duration: 700, delay: 1500 }}" href="{github}"
+                     target="_blank" class="link"><img alt="github" src="/icons/github-alt.svg"
+                                                       class="icon"/>
+                    <span class="link">GitHub</span>
+                  </a>
+                  {#if website !== ''}
+                    <a transition:fly="{{ y: 200, duration: 700, delay: 2500 }}" href="{website}"
+                       target="_blank" class="link"><img alt="website" src="/icons/web.svg"
+                                                         class="icon"/>
+                      <span class="link">Website</span>
+                    </a>
+                  {/if}
                 </div>
-            </TypeWriter>
-        {:else}
-            <div class="term">
-                <div class="term-text">{termText} </div>
-            </div>
-        {/if}
-    </footer>
-
+              {/if}
+            </div> <!-- close class details -->
+          </div> <!-- close class stuff -->
+        {/each}
+      </div> <!-- close class flex item -->
+    </div> <!-- close class flex container -->
+  {/if}
+  <footer>
+    {#if typeWriting}
+      <TypeWriter cursor=true on:done={() => OnTypeWritingFinished()}>
+        <div class="term">
+          <div class="term-text">{termText} </div>
+        </div>
+      </TypeWriter>
+    {:else}
+      <div class="term">
+        <div class="term-text">{termText} </div>
+      </div>
+    {/if}
+  </footer>
 </main>
-<style>
-
-</style>
