f222beb0784b6720dce4f85b42256c2f1ad62d28
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-11-14 16:02:01 +0000

    pagination prettified

---
+++ src/simple-portfolio/src/components/Author.svelte
@@ -0,0 +1,8 @@
+<script>
+    import AuthorCard from "./AuthorCard.svelte";
+</script>
+<div class="row">
+    <div class="col-left">
+        <AuthorCard/>
+    </div>
+</div>
---
+++ src/simple-portfolio/src/components/Home.svelte
@@ -0,0 +1,140 @@
+<script>
+    import {fly} from 'svelte/transition'
+
+    // Components
+    import Author from "./Author.svelte";
+    import Projects from "./Projects.svelte";
+    import TechStack from "./TechStack.svelte";
+
+    // Stores
+    import ProjectsStore from "../stores/ProjectsStore";
+    import {cleanTechStack} from "../stores/TechStackStore";
+
+    // Store Data
+    let projectsData
+    let hideProjects = false
+    let pageLoaded = false
+
+    // Pagination
+    let currentPage = 1
+    let projectsPerPage = 3
+    let shownProjects = []
+
+    ProjectsStore.subscribe((data) => {
+        projectsData = data
+    })
+
+    function initProjects() {
+        closeAllProjects()
+        let startIndex
+        startIndex = currentPage * projectsPerPage - 3
+
+        if (currentPage === 1) {
+            startIndex = 0
+        }
+
+        let t = projectsPerPage * currentPage
+
+        if (projectsData.length < t) {
+            shownProjects = projectsData.slice(startIndex)
+        } else {
+            shownProjects = projectsData.slice(startIndex, projectsPerPage * currentPage)
+        }
+    }
+
+    function closeAllProjects() {
+        let i
+        for (i = 0; i < shownProjects.length; i++) {
+            shownProjects[i].toggled = false
+        }
+    }
+
+    function simulateLoading(exec) {
+        setTimeout(() => {
+            exec()
+        }, 800)
+
+    }
+
+    function onPageLoaded() {
+        pageLoaded = true
+    }
+
+    function getMaxNumPages() {
+        if (projectsData.length % projectsPerPage > 0) {
+            return Math.floor(projectsData.length / projectsPerPage) + 1
+        }
+        return projectsData.length / projectsPerPage
+
+    }
+
+    function onPageSwitchedEndLoading() {
+        hideProjects = false
+    }
+
+    function nextPage() {
+        currentPage++
+        onPageSwitched()
+    }
+
+    function previousPage() {
+        currentPage--
+        onPageSwitched()
+    }
+
+    function onPageSwitched() {
+        initProjects()
+        hideProjects = true
+        cleanTechStack()
+        simulateLoading(onPageSwitchedEndLoading)
+    }
+
+    initProjects()
+    simulateLoading(onPageLoaded)
+
+
+</script>
+<main>
+    <Author/>
+    {#if pageLoaded}
+        <div class="row mt-4">
+            <div class="col-left projects pr-1 mb-2" transition:fly="{{ x: -200, duration: 300}}">
+                <div class="projects-title" transition:fly>Projects</div>
+                {#if !hideProjects}
+                    <Projects projects={shownProjects}/>
+                {/if}
+            </div>
+            <TechStack/>
+        </div>
+            <ul class="pagination">
+                <li on:click= {getMaxNumPages() !== currentPage ? nextPage : previousPage }>{currentPage}/{getMaxNumPages()}</li>
+                {#if currentPage > 1}
+                    <li on:click={previousPage}>❮</li>
+                {/if}
+                {#if getMaxNumPages() !== currentPage}
+                    <li on:click={nextPage}>❯</li>
+                {/if}
+            </ul>
+    {/if}
+</main>
+
+<style>
+    ul.pagination {
+        display: inline-block;
+        padding: 0;
+        margin: 0;
+    }
+
+    ul.pagination li {display: inline;}
+
+    ul.pagination li {
+        float: left;
+        padding: 4px 8px;
+        text-decoration: none;
+        transition: background-color .3s;
+        font-size: 18px;
+        cursor: pointer;
+    }
+
+    ul.pagination li:hover:not(.active) {background-color: #32698f;}
+</style>
